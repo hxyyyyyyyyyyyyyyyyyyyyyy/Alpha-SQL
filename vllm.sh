7c0152cd8c2783b7fb57a49f59a1618d4aa60239
cbacode <1586230797@qq.com>
2026-03-20 20:25:12 +0800

    merge

--- vllm.sh
+++ vllm.sh
@@ -1,4 +1,6 @@
 conda activate vllm
+CUDA_VISIBLE_DEVICES=1 vllm serve Qwen/Qwen3-8B --served-model-name Qwen/Qwen3-8B --port 9999 -tp 1 
+
 CUDA_VISIBLE_DEVICES=1 vllm serve Qwen/Qwen2.5-Coder-7B-Instruct --served-model-name Qwen/Qwen2.5-Coder-7B-Instruct --port 10000 -tp 1 --rope-scaling '{"rope_type":"yarn","factor":4.0,"original_max_position_embeddings":32768}' --max-model-len 131072
 
 CUDA_VISIBLE_DEVICES=0,1 vllm serve Qwen/Qwen2.5-Coder-32B-Instruct --served-model-name Qwen/Qwen2.5-Coder-32B-Instruct --port 9999 -tp 2 --rope-scaling '{"rope_type":"yarn","factor":4.0,"original_max_position_embeddings":32768}' --max-model-len 131072
